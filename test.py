7c675173e2075e1036700e7390805b4e8ea8a3be
JuanFerrer <juanemilioferrer@outlook.com>
2017-08-02 16:25:09 +0100

    Add custom parser

--- test.py
+++ test.py
@@ -1,19 +1,56 @@
-from textblob import TextBlob
+from pattern.en import parse
+from pattern.en import parsetree
+import json
 
-# text = """Oh, Misty Eye of the mountain below,
-# keep careful watch of my brothers' souls.
-# And should the sky be filled with fire and smoke,
-# keep watching over Durin's sons."""
+text1 = """Oh, misty eye of the mountain below,
+keep careful watch of my brothers' souls.
+And should the sky be filled with fire and smoke,
+keep watching over Durin's sons."""
 
-text = "The quick brown fox jumps over the lazy dog."
+text1 = "Keep careful watch of my brother's soul"
 
+text = "I love this big sky, and this dumb lizard."
+text1 = "I want to go home"
+text1 = "It had been a wonderful night"
+text1 = "Tahm is fat"
+text1 = "The quick brown fox jumped over the lazy dog"
 
-blob = TextBlob(text);
-tags = blob.tags
+# tokens = parse(text, relations=True).split()[0]
+tokens = parsetree(text, relations=True)
+sentence = {}
+nouns = []
+verbs = []
+adjectives = []
 
-# for sentence in blob.sentences:
-#     for word in sentence.words:
-#         print word;
+print text
 
-for tagPair in tags:
-    print(tagPair[0] + " is " + tagPair[1])
+for s in tokens:
+    for chunk in s.chunks:
+        print "Chunk:           " + chunk.string + "\n  Role: " + str(chunk.role) + "\n     Related to: " + str(chunk.related)
+        adjectives = []
+        if chunk.type == "VP":
+            verbs.append(chunk.string)
+        else:
+            for word in chunk.words:
+                if word.type.startswith("NN") or word.type.startswith("PRP"):
+                    nouns.append(word.string)
+                    sentence[word.string] = {}
+                    sentence[word.string]["attributes"] = adjectives
+                    sentence[word.string]["actions"] = []
+                    sentence[word.string]["actionsReceived"] = []
+                    # Decide whether the object acts or is acted upon
+                    if len(chunk.related) > 0:
+                        for r in chunk.related:
+                            if str(r.type).startswith("VP"):
+                                if str(chunk.role) == "SBJ":
+                                    sentence[word.string]["actions"].append(r.string)
+                                elif str(chunk.role) == "OBJ":
+                                    sentence[word.string]["actionsReceived"].append(r.string)
+                elif word.type.startswith("JJ"):
+                    adjectives.append(word.string)
+                elif word.type.startswith("VB"):
+                    verbs.append(word.string)
+                elif word.type.startswith("ASD"):
+                    pass
+
+print json.dumps(sentence, indent=2)
